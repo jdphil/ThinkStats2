5edbfc0d599b95c7438b3d6bc25bb7b483f5e90d
Jon Phillipo <jonathan.phillipo@rpmi.co.uk>
2018-02-26 10:54:23 +0000

    Updates to exercise1

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -8,11 +8,21 @@ License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 from __future__ import print_function
 
 import numpy as np
+import pandas as pd
 import sys
 
 import nsfg
 import thinkstats2
 
+
+def ReadFemResp(dct_file='2002FemResp.dct',
+                dat_file='2002FemResp.dat.gz'):
+    """
+
+    """
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip')
+    return df
 
 def main(script):
     """Tests the functions in this module.
@@ -24,3 +34,8 @@ def main(script):
 
 if __name__ == '__main__':
     main(*sys.argv)
+    resp = ReadFemResp()
+    preg = nsfg.ReadFemPreg()
+    preg_map = nsfg.MakePregMap(resp)
+    print(resp.pregnum.value_counts())
+    import pdb; pdb.set_trace()
