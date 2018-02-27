093eff712aaba0de54db76514b674e7fff3cb6a6
Jon Phillipo <jonathan.phillipo@rpmi.co.uk>
2018-02-27 20:19:37 +0000

    Split out to functions and made code pass pylint

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -6,41 +6,49 @@ License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 """
 
 from __future__ import print_function
-
-import numpy as np
-import pandas as pd
 import sys
 
 import nsfg
 import thinkstats2
 
 
-def ReadFemResp(dct_file='2002FemResp.dct',
-                dat_file='2002FemResp.dat.gz'):
-    """
+def read_fem_resp(dct_file='2002FemResp.dct', dat_file='2002FemResp.dat.gz'):
+    """Function to read the Female Response File.
 
+    dct_file: dictionary file
+    dat_file: data file in zip format
     """
     dct = thinkstats2.ReadStataDct(dct_file)
-    df = dct.ReadFixedWidth(dat_file, compression='gzip')
-    return df
+    df_resp = dct.ReadFixedWidth(dat_file, compression='gzip')
+    return df_resp
 
-def main(script):
+
+def check_preg_num():
+    """Function to Check that Pregnum in the response file
+        matches the number of pregnacies in the pregnancy file.
+
+    dct_file: dictionary file
+    dat_file: data file in zip format
+    """
+    for row in RESP.iterrows():
+        if row['pregnum'] != len(PREG_MAP[row['caseid']]):
+            return False
+
+    return True
+
+
+def main():
     """Tests the functions in this module.
 
     script: string script name
     """
-    print('%s: All tests passed.' % script)
+    assert check_preg_num()
+    print('%s: All tests passed.')
 
 
 if __name__ == '__main__':
+    RESP = read_fem_resp()
+    PREG = nsfg.ReadFemPreg()
+    PREG_MAP = nsfg.MakePregMap(PREG)
+    print(RESP.pregnum.value_counts())
     main(*sys.argv)
-    resp = ReadFemResp()
-    preg = nsfg.ReadFemPreg()
-    preg_map = nsfg.MakePregMap(preg)
-    print(resp.pregnum.value_counts())
-
-    for index, row in resp.iterrows():
-        if row['pregnum'] != len(preg_map[row['caseid']]):
-            print(row.caseid)
-
-    import pdb; pdb.set_trace()
